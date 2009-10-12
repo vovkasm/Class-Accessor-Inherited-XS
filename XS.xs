92233a9456feaf8817bd5d87662d8ef4a3182440
vovkasm <vovkasm@gmail.com>
2009-10-12 10:09:09 +0400

    more

--- XS.xs
+++ XS.xs
@@ -5,5 +5,67 @@
 #include "ppport.h"
 
 
-MODULE = Class::Accessor::Inherited::XS		PACKAGE = Class::Accessor::Inherited::XS		
+MODULE = Class::Accessor::Inherited::XS		PACKAGE = Class::Accessor::Inherited::XS
 
+void
+get_inherited(self,acc)
+    SV* self;
+    SV* acc;
+PROTOTYPE: DISABLE
+INIT:
+    HE* he;
+    HV* stash;
+    SV* pkg_acc;
+    AV* supers;
+    I32 len;
+    I32 key;
+PPCODE:
+{
+    warn("get_inherited: ");
+    if (sv_isobject(self)) {
+        if (SvTYPE(SvRV(self)) != SVt_PVHV)
+            croak("Oops, object not HASH based");
+
+        if (he = hv_fetch_ent( (HV *)SvRV(self), acc, 0, 0)) {
+            PUSHs( HeVAL(he) );
+            XSRETURN(1);
+        }
+
+        stash = SvSTASH(self);
+    }
+    else
+        stash = gv_stashsv(self, GV_ADD);
+
+    // Can't find in object, so try self package
+    pkg_acc = newSVpvn("__cag_",6);
+    sv_catsv(pkg_acc, acc);
+
+    /*    
+    warn("\tlookup1: %s::%s\n",HvNAME(stash),SvPV_nolen(pkg_acc));
+
+    if (he = hv_fetch_ent( stash, pkg_acc, 0, 0)) {
+        PUSHs( HeVAL(he) );
+        XSRETURN(1);
+    }
+    */
+    // Now try all superclasses
+    supers = mro_get_linear_isa(stash);
+    len = av_len(supers);
+
+    for (key = 0; key <= len; key++) {
+        SV **svp = av_fetch(supers, key, 0);
+        if (svp) {
+            SV* super = (SV *)*svp;
+            stash = gv_stashsv(super, GV_ADD);
+
+    warn("\tlookup2: %s::%s\n",HvNAME(stash),SvPV_nolen(pkg_acc));
+
+            if (he = hv_fetch_ent( stash, pkg_acc, 0, 0)) {
+                PUSHs( HeVAL(he) );
+                XSRETURN(1);
+            }
+        }
+    }
+
+    XSRETURN_UNDEF;
+}
--- lib/Class/Accessor/Inherited/XS.pm
+++ lib/Class/Accessor/Inherited/XS.pm
@@ -4,28 +4,65 @@ use 5.010001;
 use strict;
 use warnings;
 
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Class::Accessor::Inherited::XS ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
+use Carp ();
+use Scalar::Util ();
+use mro;
 
 our $VERSION = '0.01';
+
+sub pp_get_inherited {
+    my $class;
+
+    if (Scalar::Util::blessed $_[0]) {
+        my $reftype = Scalar::Util::reftype $_[0];
+        $class = ref $_[0];
+
+        if ($reftype eq 'HASH' && exists $_[0]->{$_[1]}) {
+            return $_[0]->{$_[1]};
+        } elsif ($reftype ne 'HASH') {
+            Carp::croak('Cannot get inherited value on an object instance that is not hash-based');
+        };
+    } else {
+        $class = $_[0];
+    };
+
+    no strict 'refs';
+    no warnings qw/uninitialized/;
+    return ${$class.'::__cag_'.$_[1]} if defined(${$class.'::__cag_'.$_[1]});
+
+    # we need to be smarter about recalculation, as @ISA (thus supers) can very well change in-flight
+    my $pkg_gen = mro::get_pkg_gen ($class);
+    if ( ${$class.'::__cag_pkg_gen'} != $pkg_gen ) {
+        @{$class.'::__cag_supers'} = $_[0]->get_super_paths;
+        ${$class.'::__cag_pkg_gen'} = $pkg_gen;
+    };
+
+    foreach (@{$class.'::__cag_supers'}) {
+        return ${$_.'::__cag_'.$_[1]} if defined(${$_.'::__cag_'.$_[1]});
+    };
+
+    return undef;
+}
+
+sub set_inherited {
+    if (Scalar::Util::blessed $_[0]) {
+        if (Scalar::Util::reftype $_[0] eq 'HASH') {
+            return $_[0]->{$_[1]} = $_[2];
+        } else {
+            Carp::croak('Cannot set inherited value on an object instance that is not hash-based');
+        };
+    } else {
+        no strict 'refs';
+
+        return ${$_[0].'::__cag_'.$_[1]} = $_[2];
+    };
+}
+
+sub get_super_paths {
+    my $class = Scalar::Util::blessed $_[0] || $_[0];
+
+    return @{mro::get_linear_isa($class)};
+}
 
 require XSLoader;
 XSLoader::load('Class::Accessor::Inherited::XS', $VERSION);
