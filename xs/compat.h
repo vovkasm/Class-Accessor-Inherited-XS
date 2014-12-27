d06aa401f2a6c23736bcd49e10ba93594ce8cd2d
Sergey Aleynikov <sergey.aleynikov@gmail.com>
2014-12-27 16:10:00 +0300

    no longer need this record in compat.h

--- xs/compat.h
+++ xs/compat.h
@@ -14,8 +14,4 @@
 #define gv_init_sv(gv, stash, sv, flags) gv_init(gv, stash, SvPVX(sv), SvLEN(sv), flags | SvUTF8(sv))
 #endif
 
-#ifndef gv_init_pvn
-#define gv_init_pvn(gv, stash, name, len, flags) gv_init(gv, stash, name, len, 0)
-#endif
-
 #endif
