ae314bccf1e6070fe691af7a863d3b44a5b13008
Sergey Aleynikov <sergey.aleynikov@gmail.com>
2015-11-29 23:39:51 +0300

    use typedef from core

--- xs/accessor_impl.h
+++ xs/accessor_impl.h
@@ -164,11 +164,10 @@ OP *
 CAIXS_entersub(pTHX) {
     dSP;
 
-    /* some older gcc's can't deduce correct function - have to add explicit cast  */
-    typedef void (*XSPROTO_CB)(pTHX_ CV*);
-
     CV* sv = (CV*)TOPs;
-    if (sv && (SvTYPE(sv) == SVt_PVCV) && (CvXSUB(sv) == (XSPROTO_CB)&CAIXS_entersub_wrapper<type>)) {
+
+    /* some older gcc's can't deduce correct function - have to add explicit cast  */
+    if (sv && (SvTYPE(sv) == SVt_PVCV) && (CvXSUB(sv) == (XSUBADDR_t)&CAIXS_entersub_wrapper<type>)) {
         /*
             Assert against future XPVCV layout change - as for now, xcv_xsub shares space with xcv_root
             which are both pointers, so address check is enough, and there's no need to look into op_flags for CvISXSUB.
