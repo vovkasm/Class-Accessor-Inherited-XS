338630ae6a5560e7cafcee42f85f64c9c18a88b6
Sergey Aleynikov <sergey.aleynikov@gmail.com>
2017-08-13 22:39:47 +0300

    Add deprecation warning for non-compat inheritance

--- lib/Class/Accessor/Inherited/XS/Compat.pm
+++ lib/Class/Accessor/Inherited/XS/Compat.pm
@@ -3,10 +3,21 @@ use 5.010001;
 use strict;
 use warnings;
 
+use Class::Accessor::Inherited::XS;
+
 sub mk_type_accessors {
     my ($class, $type) = (shift, shift);
 
-    my ($installer, $clone_arg) = $class->_type_installer($type);
+    {
+        require mro;
+        state $seen = {};
+        state $message = <<EOF;
+Inheriting from 'Class::Accessor::Inherited::XS' is deprecated, this behavior will be removed in the next version! To use __PACKAGE__->mk_${type}_accessors form inherit from 'Class::Accessor::Inherited::XS::Compat' instead.
+EOF
+        warn $message if !$seen->{$class}++ && scalar grep { $_ eq 'Class::Accessor::Inherited::XS' } @{ mro::get_linear_isa($class) };
+    }
+
+    my ($installer, $clone_arg) = Class::Accessor::Inherited::XS->_type_installer($type);
 
     for my $entry (@_) {
         if (ref($entry) eq 'ARRAY') {
