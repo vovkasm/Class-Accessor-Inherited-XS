3a98ec322cff88c0f64fb3a1f5d7c0804fc276ef
Sergey Aleynikov <sergey.aleynikov@gmail.com>
2017-08-13 23:56:56 +0300

    release 0.34 - manifest fix

--- lib/Class/Accessor/Inherited/XS.pm
+++ lib/Class/Accessor/Inherited/XS.pm
@@ -7,7 +7,7 @@ use Class::Accessor::Inherited::XS::Compat qw/mk_type_accessors mk_inherited_acc
 
 use Carp ();
 
-our $VERSION = '0.33';
+our $VERSION = '0.34';
 our $PREFIX  = '__cag_';
 
 require XSLoader;
